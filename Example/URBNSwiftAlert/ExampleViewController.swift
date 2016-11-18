d9d46952e2d3abe2735bbafec676665ce4fc6c1a
Kevin Taniguchi <ktaniguchi@urbn.com>
2016-11-18 15:16:13 -0600

    remove sboard

---
+++ Example/URBNSwiftAlert/ExampleViewController.swift
@@ -0,0 +1,14 @@
+//
+//  ExampleViewController.swift
+//  URBNSwiftAlert
+//
+//  Created by Kevin Taniguchi on 11/18/16.
+//  Copyright © 2016 CocoaPods. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ExampleViewController: UIViewController {
+    
+}
